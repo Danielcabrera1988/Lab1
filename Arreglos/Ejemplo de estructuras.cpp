40b9a34fc41edf6c2de89c137946ce516611237c
Cabrera Daniel <71732188+Danielcabrera1988@users.noreply.github.com>
2021-09-12 01:48:50 -0300

    Update Ejemplo de estructuras.cpp

--- Arreglos/Ejemplo de estructuras.cpp
+++ Arreglos/Ejemplo de estructuras.cpp
@@ -1,11 +1,23 @@
 #include <stdio.h>
-#include <string.h>struct amigo{	int id;	char nombre[20];	char dir[20];};
+#include <string.h>
+struct amigo
+{
+	int id;
+	char nombre[20];
+	char dir[20];
+};
 
 int main(int argc, char *argv[]) {
 	
-	struct amigo record = {0}; //declarar la estructura	// asignar datos a cada elemento de la estructura	record.id=1;	strcpy(record.nombre, "Raju");	strcpy(record.dir, "catamarca 30");	// mostrar por pantalla los datos de la estructura	printf(" Id es: %d \n", record.id);	printf(" Name es: %s \n", record.nombre);	printf(" Dirección es: %s \n", record.dir);
-	
+	struct amigo record = {0}; //declarar la estructura
+	// asignar datos a cada elemento de la estructura
+	record.id=1;
+	strcpy(record.nombre, "Raju");
+	strcpy(record.dir, "catamarca 30");
+	// mostrar por pantalla los datos de la estructura
+	printf(" Id es: %d \n", record.id);
+	printf(" Name es: %s \n", record.nombre);
+	printf(" DirecciÃ³n es: %s \n", record.dir);
 	
 	return 0;
 }
-
