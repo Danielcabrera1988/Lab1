3ebd5832c7d5f4bec5d0b1acef4f1c059e56240a
Cabrera Daniel <71732188+Danielcabrera1988@users.noreply.github.com>
2021-09-12 01:49:15 -0300

    Update Ejemplo strcat.cpp

--- Arreglos/Ejemplo strcat.cpp
+++ Arreglos/Ejemplo strcat.cpp
@@ -3,13 +3,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	char texto1[10]; 	char texto2[10]; 	char texto3[10];	
+	char texto1[10]; 
+	char texto2[10]; 
+	char texto3[10];
+	
 	printf("Ingrese su Nombre: ");
-	scanf(" %s", &texto1);	printf("Ingrese su Apellido: ");
+	scanf(" %s", &texto1);
+	printf("Ingrese su Apellido: ");
 	scanf(" %s", &texto2);
 	
-	strcat(texto1,texto2); 	printf("%s\n",texto1); 		getchar();	return 0;
+	strcat(texto1,texto2); 
+	printf("%s\n",texto1); 
+	
+	getchar();
+	return 0;
 	
 	return 0;
 }
-
