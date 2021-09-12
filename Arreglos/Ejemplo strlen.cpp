49f52cdda544f9ff99b4982f7e0cf775581aeef1
Cabrera Daniel <71732188+Danielcabrera1988@users.noreply.github.com>
2021-09-12 01:50:34 -0300

    Update Ejemplo strlen.cpp

--- Arreglos/Ejemplo strlen.cpp
+++ Arreglos/Ejemplo strlen.cpp
@@ -1,2 +1,15 @@
-#include <stdio.h> #include <string.h> #include <conio.h> #define MAXLON 80 int main(){ 	char a[MAXLON+1]; 	int longitud; 	printf ("Introduce una cadena (max. %d caracteres): ", MAXLON);	scanf("%s",&a); 	longitud = strlen(a); 	printf ("\nLongitud de la cadena: %d\n", longitud); 	getch();	return 0; } 
-
+#include <stdio.h> 
+#include <string.h> 
+#include <conio.h> 
+#define MAXLON 80 
+int main()
+{ 
+	char a[MAXLON+1]; 
+	int longitud; 
+	printf ("Introduce una cadena (max. %d caracteres): ", MAXLON);
+	scanf("%s",&a); 
+	longitud = strlen(a); 
+	printf ("\nLongitud de la cadena: %d\n", longitud); 
+	getch();
+	return 0; 
+} 
