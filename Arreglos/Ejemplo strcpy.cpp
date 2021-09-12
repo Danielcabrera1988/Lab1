724b7e4d543a464ed9ab6a69b3ef20c686c31cd4
Cabrera Daniel <71732188+Danielcabrera1988@users.noreply.github.com>
2021-09-12 01:50:19 -0300

    Update Ejemplo strcpy.cpp

--- Arreglos/Ejemplo strcpy.cpp
+++ Arreglos/Ejemplo strcpy.cpp
@@ -3,10 +3,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	char texto1[]="corta"; 	char texto2[]="mediana"; 	char texto3[]="larguisima"; 
-		strcpy(texto2,texto1); 	printf("%s\n",texto2); 
-		strcpy(texto2,texto3); 	printf("%s\n",texto2);  
+	char texto1[]="corta"; 
+	char texto2[]="mediana"; 
+	char texto3[]="larguisima"; 
+	
+	strcpy(texto2,texto1); 
+	printf("%s\n",texto2); 
+	
+	strcpy(texto2,texto3); 
+	printf("%s\n",texto2); 
 	
 	return 0;
 }
-
